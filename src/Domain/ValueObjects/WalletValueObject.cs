68d7084c43775486faf6173ec43d0bd6df9b0abf
david <d.gogodze@hotmail.com>
2024-08-04 17:12:06 +0400

    finished balance implementation

--- src/Domain/ValueObjects/WalletValueObject.cs
+++ src/Domain/ValueObjects/WalletValueObject.cs
@@ -1,19 +1,43 @@
 using Domain.Aggregates;
+
 namespace Domain.ValueObjects;
 
-public record Wallet(decimal Balance){
+public record Wallet(decimal Balance)
+{
     public User User { get; set; }
-    private decimal Balance { get; set; } = Balance;
-    
+    private decimal _balance = default;
+
+    public decimal Balance
+    {
+        get => _balance;
+        set
+        {
+            if (value < 0)
+            {
+                throw new Exception("number cant be negative");
+            }
+
+            _balance += value;
+        }
+    }
+
     public bool HasBalance(decimal amount)
     {
         if (amount == Balance) return true;
         return false;
     }
 
-    public bool TryTranfer(Wallet other)
+    public bool TryTranfer(Wallet other, decimal amount)
     {
-          //todo add transfer logic
-          throw new NotImplementedException();
+        // check if user has enough balance if not return false
+        if (Balance - amount < 0)
+        {
+            return false;
+        }
+
+        // if users balance won't be negative after we deduct, then we can say he has enough balance, so we can transfer the money 
+        other.Balance += amount;
+        Balance = Balance - amount;
+        return true;
     }
 }
