cd8a562a0cb6280be07f3dea8f91158fb283e82b
ddjerqq <ddjerqq@github.com>
2024-08-07 23:38:43 +0400

    added migration: Add wallet to user

---
+++ src/Persistence/Migrations/20240807193813_AddWalletToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWalletToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "wallet",
+                table: "user",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "wallet",
+                table: "user");
+        }
+    }
+}
