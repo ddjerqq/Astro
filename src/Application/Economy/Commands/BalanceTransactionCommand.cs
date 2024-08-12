90dd6fdcf74f08cbaa50d8f7a57bb69c83433c58
david <d.gogodze@hotmail.com>
2024-08-12 19:33:51 +0400

    fied for review

--- src/Application/Economy/Commands/BalanceTransactionCommand.cs
+++ src/Application/Economy/Commands/BalanceTransactionCommand.cs
@@ -1,5 +1,6 @@
 using Application.Services;
 using Application.Users.Commands;
+using Astro.Generated;
 using Domain.Aggregates;
 using Domain.Abstractions;
 using Microsoft.EntityFrameworkCore;
@@ -8,13 +9,16 @@ using MediatR;
 namespace Application.Economy.Commands;
 
 
-public sealed record BalanceTransactionCommand(User sender, User reciver, decimal amount) : IRequest<bool>;
+public sealed record BalanceTransactionCommand(UserId sender, UserId reciver, decimal amount) : IRequest<bool>;
 
 public sealed class TransactionValidator : AbstractValidator<BalanceTransactionCommand>
 {
     public TransactionValidator()
     {
-        RuleFor(x => x.amount).LessThanOrEqualTo(0).WithMessage("amount can not be 0 or less");
+        RuleSet("async", () => { RuleFor(x => x.amount)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("amount can not be 0 or less");});
+       
     }
 }
 
@@ -23,9 +27,11 @@ public sealed class TransactionValidator : AbstractValidator<BalanceTransactionC
 public sealed class BalanceTransactionCommandHandler(IAppDbContext dbContext, CancellationToken ct)
     : IRequestHandler<BalanceTransactionCommand, bool>
 {
-    public Task<bool> Handle(BalanceTransactionCommand request,CancellationToken ct)
+    public Task<bool> Handle(BalanceTransactionCommand request, CancellationToken ct)
     {
         // request.reciver.Wallet.Balance + request.amount;
         // request.sender.Wallet.Balance - request.amount;
+        dbContext.Set<User>();
     };
-};
+
+}
