b5f2765019def8b9e2fc4f4ee8e2e8a3cf27301e
dgogo <d.gogodze@hotmail,com>
2024-08-17 18:43:52 +0400

    implemented transaction command

--- src/Application/Economy/Commands/BalanceTransactionCommand.cs
+++ src/Application/Economy/Commands/BalanceTransactionCommand.cs
@@ -6,32 +6,42 @@ using Domain.Abstractions;
 using Microsoft.EntityFrameworkCore;
 using FluentValidation;
 using MediatR;
+
 namespace Application.Economy.Commands;
 
-
-public sealed record BalanceTransactionCommand(UserId sender, UserId reciver, decimal amount) : IRequest<bool>;
+public sealed record BalanceTransactionCommand(UserId SenderId, UserId ReceiverId, decimal Amount) : IRequest<bool>;
 
 public sealed class TransactionValidator : AbstractValidator<BalanceTransactionCommand>
 {
     public TransactionValidator()
     {
-        RuleFor(x => x.amount)
+        RuleFor(x => x.Amount)
             .LessThanOrEqualTo(0)
             .WithMessage("amount can not be 0 or less");
-       
     }
 }
 
-
-
-public sealed class BalanceTransactionCommandHandler(IAppDbContext dbContext, CancellationToken ct)
+public sealed class BalanceTransactionCommandHandler(IAppDbContext dbContext)
     : IRequestHandler<BalanceTransactionCommand, bool>
 {
-    public Task<bool> Handle(BalanceTransactionCommand request, CancellationToken ct)
+    public async Task<bool> Handle(BalanceTransactionCommand request, CancellationToken ct)
     {
-        // request.reciver.Wallet.Balance + request.amount;
-        // request.sender.Wallet.Balance - request.amount;
-        // dbContext.Set<User>();
-    };
+        var sender = await dbContext.Set<User>().FindAsync(request.SenderId, ct);
+        var receiver = await dbContext.Set<User>().FindAsync(request.ReceiverId, ct);
+        // if one of the users is null throw an exception
+        if (sender is null || receiver is null)
+        {
+            throw new InvalidOperationException("sender or receiver is invalid");
+        }
 
+        if (sender.Wallet.TryTransfer(receiver.Wallet, request.Amount))
+        {
+            dbContext.Set<User>().Update(receiver);
+            dbContext.Set<User>().Update(sender);
+            await dbContext.SaveChangesAsync(ct);
+            return true;
+        }
+        
+        return false;
+    }
 }
