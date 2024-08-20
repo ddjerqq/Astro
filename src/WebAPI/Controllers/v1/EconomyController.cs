a85b557935f64316e27e8e4da59e4a33235c729e
HexDev <david@gogodze.dev>
2024-08-20 19:11:39 +0400

    added transactions api controller

---
+++ src/WebAPI/Controllers/v1/EconomyController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Economy.Commands;
+using Astro.Generated;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1;
+
+[Route("/api/v1/transfer")]
+public class EconomyController(IMediator mediator) : ApiController
+{
+    [HttpPost("/send/{ReciverId}")]
+    public Task<bool> Send(UserId ReciverId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
